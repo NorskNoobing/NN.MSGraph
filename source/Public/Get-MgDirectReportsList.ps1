394271be3a819e52fc51e4dc23d6616e22e79d5d
NorskNoobing <39239702+NorskNoobing@users.noreply.github.com>
2022-12-15 08:28:22 +0100

    Update output of direct reports function

--- source/Public/Get-MgDirectReportsList.ps1
+++ source/Public/Get-MgDirectReportsList.ps1
@@ -1,13 +1,18 @@
 function Get-MgDirectReportsList {
+    [CmdletBinding()]
     param (
         [Parameter(Mandatory)][string]$identifier
     )
-    $splat = @{
-        "Method" = "GET"
-        "Uri" = "https://graph.microsoft.com/v1.0/users/$identifier/directReports"
-        "Headers" = @{
-            "Authorization" = "Bearer $(Get-MgAccessToken)"
+
+    process {
+        $splat = @{
+            "Method" = "GET"
+            "Uri" = "https://graph.microsoft.com/v1.0/users/$identifier/directReports"
+            "Headers" = @{
+                "Authorization" = "Bearer $(Get-MgAccessToken)"
+            }
         }
+        $Result = Invoke-RestMethod @splat
+        $Result.value
     }
-    Invoke-RestMethod @splat
 }
