87158533120c0ea6796e9c428b17a2c9545844d5
NorskNoobing <39239702+NorskNoobing@users.noreply.github.com>
2023-03-09 15:56:49 +0100

    Update Get-MgShiftList.ps1
     * Make timeframe optional
    * Add functionality to get shifts on all pages instead of being limited to 1000 shifts

--- source/Public/Get-MgShiftList.ps1
+++ source/Public/Get-MgShiftList.ps1
@@ -1,28 +1,52 @@
 function Get-MgShiftList {
+    [CmdletBinding(DefaultParameterSetName="List shifts")]
     param (
         #Id of the Team to get shifts from
         [Parameter(Mandatory)]$teamId,
         #Id of the user that the request is sent on the behalf of
         [Parameter(Mandatory)]$actAsUID,
         #Timespan to fetch shifts from
-        [Parameter(Mandatory)][datetime]$dateFrom,
-        [datetime]$dateTo = $dateFrom.AddDays(1)
+        [Parameter(ParameterSetName="Get shifts within timeframe",Mandatory)][datetime]$dateFrom,
+        [Parameter(ParameterSetName="Get shifts within timeframe")][datetime]$dateTo,
+        [Parameter(ParameterSetName="List shifts")][switch]$ListShifts
     )
 
-    #Convert to "ISO 8601" date format, which is supported in json queries
-    $convertedDateFrom = [Xml.XmlConvert]::ToString($dateFrom,[Xml.XmlDateTimeSerializationMode]::Utc)
-    $convertedDateTo = [Xml.XmlConvert]::ToString($dateTo,[Xml.XmlDateTimeSerializationMode]::Utc)
+    switch ($PsCmdlet.ParameterSetName) {
+        "Get shifts within timeframe" {
+            if (!$dateTo) {
+                $dateTo = $dateFrom.AddDays(1)
+            }
+            
+            #Convert to "ISO 8601" date format, which is supported in json queries
+            $convertedDateFrom = [Xml.XmlConvert]::ToString($dateFrom,[Xml.XmlDateTimeSerializationMode]::Utc)
+            $convertedDateTo = [Xml.XmlConvert]::ToString($dateTo,[Xml.XmlDateTimeSerializationMode]::Utc)
 
-    $splat = @{
+            $splat += @{
+                "Body" = @{
+                    '$filter' = @"
+sharedShift/startDateTime ge $convertedDateFrom and sharedShift/endDateTime le $convertedDateTo
+"@
+                }
+            }
+        }
+    }
+
+    $splat += @{
         "Method" = "GET"
         "Uri" = "https://graph.microsoft.com/v1.0/teams/$teamId/schedule/shifts"
         "Headers" = @{
             "Authorization" = "Bearer $(Get-MgAccessToken)"
             "MS-APP-ACTS-AS" = $actAsUID
         }
-        "Body" = @{
-            '$filter' = "sharedShift/startDateTime ge $convertedDateFrom and sharedShift/endDateTime le $convertedDateTo"
-        }
     }
-    Invoke-RestMethod @splat
+    $result = Invoke-RestMethod @splat
+    $ExportObjects += $result.value
+
+    while ($result.'@odata.nextLink') {
+        $splat.Uri = $result.'@odata.nextLink'
+        $result = Invoke-RestMethod @splat
+        $ExportObjects += $result.value
+    }
+    
+    Write-Output $ExportObjects
 }
